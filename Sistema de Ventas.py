48d0b54bc5b457ebbd0e5164222e4ff3d376bafc
Cristian Chete <cristiancheter@gmail.com>
2025-02-28 11:20:11 -0600

    git push
    Signed-off-by: Cristian Chete <cristiancheter@gmail.com>

--- Sistema de Ventas.py
+++ Sistema de Ventas.py
@@ -1,18 +1,108 @@
 
-sdsd
+
 import os
 import tkinter as tk
 from tkinter import Menu
 from tkinter import messagebox
 from tkinter import ttk,filedialog
-
+#archivo donde se almacenen los usuarios
+ARCHIVO_CLIENTES = "clientes.txt"
 DATA_FILE="Vendedores.txt"#archivo que guarda datos de vendedores
 
 def focus_next_widget(event):
     event.widget.tk_focusNext().focus()
     return "break"
 
+def registrar_cliente ():
+    id = entry_id.get()
+    cliente = entry_cliente.get()
+    direccion = entry_direccion.get()
+    telefono = entry_telefono.get()
+    email = entry_email.get()
 
+    if id and cliente:
+        #verifica si el usuario ya existe
+        if verificar_cliente_existente(cliente):
+            messagebox.showerror("Error", "el cliente ya existe")
+            return
+        with open(ARCHIVO_CLIENTES, "a")as file:
+            file.write(f"{id},{cliente},{direccion},{telefono},{telefono},{email}\n")
+
+        messagebox.showinfo("Exito", "Cliente registrado correctamente.")
+        entry_id.delete(0,tk.END)
+        entry_cliente.delete(0,tk.END)
+        entry_direccion.delete(0, tk.END)
+        entry_telefono.delete(0, tk.END)
+        entry_email.delete(0, tk.END)
+
+    else:
+        messagebox.showerror("Error", "Por favor, complete todos los campos.")
+
+def verificar_cliente_existente(cliente):
+    if os.path.exists(ARCHIVO_CLIENTES):
+        with open(ARCHIVO_CLIENTES,"r") as file:
+            for linea in file:
+                datos = linea.strip().split(",")
+                if datos[0] == cliente:
+                    return True
+    return False
+
+def mostrar_clientes():
+    if os.path.exists(ARCHIVO_CLIENTES):
+        with open(ARCHIVO_CLIENTES,"r")as file:
+            lista_clientes.delete("1.0",tk.END)# El valor "1.0" especifica la posicion inicial del texto a eliminar, 1 es la primera linea y 0 la primera columna
+            for linea in file:
+                    datos = linea.strip().split(",")
+                    lista_clientes.insert(tk.END,F"Cliente: ID:{datos[0]}, Nombre:{datos[1]}, Direccion:{datos[2]}, Tel.{datos[3]}, Correo:{datos[5]}\n")
+
+    else:
+        messagebox.showerror("Error","No hay clientes registrados")
+
+
+
+#configuracion de la interfaz grafica
+
+root = tk.Tk()
+root.title("Gestion de Clientes")
+root.geometry("650x650")
+
+
+#eiquetas y entradas de usuarios
+
+tk.Label(root,text="ID Cliente:").pack() #<-Posiciona el widget en la ventana de manera automatica
+entry_id=tk.Entry(root)
+entry_id.pack()
+
+tk.Label(root,text="Nombre Cliente: ").pack()
+entry_cliente=tk.Entry(root)
+entry_cliente.pack()
+
+tk.Label(root,text="Direccion: ").pack()
+entry_direccion=tk.Entry(root)
+entry_direccion.pack()
+
+tk.Label(root,text="Telefono: ").pack()
+entry_telefono=tk.Entry(root)
+entry_telefono.pack()
+
+tk.Label(root,text="E-Mail: ").pack()
+entry_email=tk.Entry(root)
+entry_email.pack()
+
+#botones de accion
+def hola():
+    pass
+
+tk.Button(root,text="Registrar Cliete",command=registrar_cliente).pack(pady=5)
+tk.Button(root,text="Modificar Informacion de cliente existente",command=hola).pack(pady=5)
+tk.Button(root,text="Eliminar Cliente",command=hola).pack(pady=5)
+tk.Button(root,text="Buscar Cliente ",command=hola).pack(pady=5)
+tk.Button(root,text="Mostrar Clintes",command=mostrar_clientes).pack(pady=5)
+
+#area de texto para listar usuarios
+
+lista_clientes = tk.Text(root,height=10,width=100)
+lista_clientes.pack(pady=5)
 
 productos = []
 
